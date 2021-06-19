59486cf8c257cdc50eac228cae4d8b78bbffeb10
nolu04 <noel.baechler04@gmail.com>
2021-06-19 14:06:44 +0200

    init

---
+++ src/main/java/nolu04/quizlearn/QuizlearnApplication.java
@@ -0,0 +1,13 @@
+package nolu04.quizlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nolu04/quizlearn/QuizlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package nolu04.quizlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
