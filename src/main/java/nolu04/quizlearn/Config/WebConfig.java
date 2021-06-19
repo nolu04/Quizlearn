d7c9e2e904d8d0ce96ec3cd59dcdc736e10bcdfd
nolu04 <noel.baechler04@gmail.com>
2021-06-19 16:44:04 +0200

    error page works

--- src/main/java/nolu04/quizlearn/Config/WebConfig.java
+++ src/main/java/nolu04/quizlearn/Config/WebConfig.java
@@ -1,11 +1,13 @@
 package nolu04.quizlearn.Config;
 
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -13,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/home").setViewName("forward:/index.html");
         registry.addViewController("/detail").setViewName("forward:/detail.html");
 //        registry.addViewController("/**").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+
     }
 }
